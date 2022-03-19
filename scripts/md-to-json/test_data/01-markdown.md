f6f532fe0d0585017ba5fc6a55f0b486dc87c2f7
Pablo Seminario <pablo@seminar.io>
2022-03-19 12:16:20 +0100

    Python script to convert Markdown posts into JSON
    It's needed because reliability.re uses Hugo.io to generate JSON files
    that will be parsed by Telegram and Twitter scripts.
    So in order to keep those scripts unchanged we need to generate JSON as
    well.

---
+++ scripts/md-to-json/test_data/01-markdown.md
@@ -0,0 +1,10 @@
+---
+title: "An example"
+date: 2020-10-09T06:51:43Z
+github_username: pabluk
+twitter_username: pabluk
+link: https://twitter.com/SREFrance
+hashtags:
+---
+This is the content.
+
---
+++ scripts/md-to-json/test_main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2022 SRE France
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from main import md_to_json
+
+
+class TestMDtoJSON(unittest.TestCase):
+    def test_basic(self):
+        """A basic test for the Happy Path."""
+        with open("test_data/01-markdown.md", "r") as f:
+            markdown = f.read()
+        with open("test_data/01-expected.json", "r") as f:
+            expected = f.read().strip()
+        json_data = md_to_json(markdown)
+        self.assertEqual(json_data, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
